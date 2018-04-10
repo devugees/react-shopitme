e3b701a90ec20ce4a20b56f1a20c65b0335ce01c
Tarek <tarek.alkhatib1@gmail.com>
2018-04-10 12:05:13 +0200

    create navbar

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 
 // import Components
 import LandingPage from './LandingPage';
+import Navbar from './Navbar';
+
 
 export default class Main extends Component {
   render() {
     return (
       <React.Fragment>
+        <Navbar />
         <LandingPage />
       </React.Fragment>
     )
