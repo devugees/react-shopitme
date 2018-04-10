0ebce62d48d00554a8fc3cdf85345a854ddbbb69
laithmassoud <laythmassoud8@gmail.com>
2018-04-10 12:03:06 +0200

    :

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 
 // import Components
 import Main from './components/Main';
+import Footer from './components/Footer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Main />
+        <Footer className="footer" />
       </div>
     );
   }
