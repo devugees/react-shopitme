9be6bfb7ebebb13bf9c1a27b7163a23b1df64030
laithmassoud <laythmassoud8@gmail.com>
2018-04-17 11:44:14 +0200

    Additional-notes

--- frontend/src/components/Additional-Notes/Notes.js
+++ frontend/src/components/Additional-Notes/Notes.js
@@ -1,39 +1,14 @@
 import React, { Component } from 'react';
-import Paper from 'material-ui/Paper';
-import TextField from 'material-ui/TextField';
+
 import './notes.css';
 
-
-
-
 export default class TextFields extends Component {
-  state = {
-    name: 'Add your Notes'
-  };
-
-  handleChange = name => event => {
-    this.setState({
-      [name]: event.target.value,
-    });
-  };
-
-  
-
-  render() {
-    return (
-
-     <Paper className='notes' elevation={4}>
-        <TextField
-        label="Add your Notes"
-        InputProps={{
-          disableUnderline: true,
-          
-        }}
-        InputLabelProps={{
-          shrink: true
-        }}
-      />
-      </Paper>
+    render() {  
+      return (
+      <div className="notes">
+        <h4>Notes:</h4>
+        <textarea className="textarea" rows="8"></textarea>
+      </div>
     );
   }
 }
