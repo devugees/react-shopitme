e2d936ea034674500aff76cc37f3616dd17f7b35
Nacho <nacho@nachomerino.com>
2018-04-19 12:15:04 +0200

    Testing area, fakeStore implemented
    with /main we can test components and we have a common fakestore

--- frontend/src/components/master-components/CreateShoppingList.js
+++ frontend/src/components/master-components/CreateShoppingList.js
@@ -5,50 +5,12 @@ import ShoppingListTitle from '../shopping-list-title/ShoppingListTitle';
 import Notes from '../Additional-Notes/Notes';
 import Button from 'material-ui/Button';
 import Details from '../Details/Details'
+//import fake store
+import fakeStore from '../../fakeStore';
 
 export default class CreateShoppingList extends Component {
 
-    state = {
-  listId: 3323,
-  shopper:{
-    name: 'Alice Doe',
-    accountPage: 'user323223'
-  },
-  orderer:{
-    name: 'Bob Doe',
-    accountPage: 'user324332',
-    coords:{
-      lat: 52.522955,
-      lng: 13.477175,
-    },
-    deliverAdress:{
-      street: 'Sonnenallee',
-      number: '154',
-      postalCode: '12055',
-      city: 'Berlin'
-    },
-  },
-  deliveringTime:{
-    start: '14:00',
-    end:'16:00'
-  },
-  items:
-  [{
-    status:'box',
-    todo:"2x Corn Bread"
-  },{
-    status:'box',
-    todo:"Kellogs AllBran"
-  },{ 
-    status:'box',
-    todo:"4x Milk 3.8% Fet"
-  },{
-    status:'box',
-    todo:"2x Orange Juice low sugar"
-  }
-  ],
-  notes:'Bring me all in a box please. Thank you',
-}
+    state = {...fakeStore}
     
     
       render() {
