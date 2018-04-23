f59d8dd21117348174dbd63fa094bc49e9a8d164
Versioncrowd <Hi@His-MacBook-Pro.local>
2018-04-24 01:41:09 +0200

    continued

--- frontend/src/components/landing/Landing.js
+++ frontend/src/components/landing/Landing.js
@@ -2,33 +2,51 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './Landing.css'
 import leadimg from '../../pictures/landing.jpg';
-import leadmobimg from '../../pictures/landing_mob.png';
+import leadmobimg from '../../pictures/landing1.jpg';
+import buttonw from '../../pictures/button_woman.png';
+import buttonm from '../../pictures/button_man.png';
+import Grid from 'material-ui/Grid';
+import Typography from 'material-ui/Typography';
 
 export default class LandingPage extends Component {
   render() {
     return (
       <div className="landing">
-        <div className="primary">
-            <img src={leadmobimg} />
-        </div>
-        <div className="deliverit_slogan" >
-            <div className="slogantxt" >
-                Need some shopping delivered, or have time to shop for someone else?
+            <div className="deliverit_slogan" >
+                Peer to peer <br/>delivery made easy!
+                <div className="chooserole" >
+                <Grid container spacing={0}>
+                    <Grid item xs={12}>
+                        <Link to={"/createshoppinglist"} >
+                            <div className="order">
+                                <div className="btn_txt">
+                                    <Typography variant="title" color="inherit">
+                                        I need some shopping.
+                                    </Typography>
+                                </div>
+                            </div>
+                        </Link>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Link to={"/createshoppinglist"} >
+                            <div className="delivery">
+                                <div className="btn_txt">
+                                    <Link to={"/accepteddelivery"} >
+                                    <Typography variant="title" color="inherit">
+                                        I want to shop for someone.
+                                        </Typography>
+                                    </Link>
+                                </div>
+                            </div>
+                        </Link>
+                    </Grid>
+                </Grid>
+                </div>
             </div>
-        </div>
-        <div className="chooserole" >
-            <div class="order">
-                <Link to={"/createshoppinglist"} >
-                    I need some groceries.
-                </Link>
+            <div className="secondary">
+                Create Your Shopping List!
+                <img src="" />
             </div>
-            <div class="deliver">
-                <Link to={"/accepteddelivery"} >
-                    I want to help someone with his shopping.
-                </Link>
-            </div>
-
-        </div>
       </div>
     )
   }
