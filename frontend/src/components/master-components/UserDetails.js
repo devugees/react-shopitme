7fdbbf1df571a57bfe286d8181106cfc10218089
Sabine Gottfried <sabine.gottfried@gmail.com>
2018-04-24 16:58:34 +0200

    added Ajax helper function

--- frontend/src/components/master-components/UserDetails.js
+++ frontend/src/components/master-components/UserDetails.js
@@ -5,6 +5,8 @@ import RatingStars from '../RatingStars';
 import EditUser from '../edit-user/EditUser';
 import fakeStore from '../../fakeStore';
 
+import { crudAPI } from './../../helpers/helpers';
+
 export default class UserDetails extends Component {
   state = { 
     ...fakeStore.orderer
@@ -18,9 +20,16 @@ export default class UserDetails extends Component {
 
   handleSubmit = formtype => event => {
     event.preventDefault();
-    const userDetails = {...this.state}; // Make a copy of state
+    // const userDetails = {...this.state}; // Make a copy of state
+    const userDetails = {
+      "email": "sabine.gottfr1231231@gmail.com",
+      "password": "123"
+    }
+
     if (formtype === "register") {
-      console.log("send the Data to the Backend-Route Register")
+      crudAPI("POST", "http://localhost:4000/register", userDetails)
+      .then(response => console.log(response))
+      
     } else if (formtype === "changeuserdetails") {
       console.log("send the Data to the Backend-Route Changeuserdetails")
     } else { console.log("form type must be specified")}
