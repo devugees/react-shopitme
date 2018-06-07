4ddb255d260c25171f22badc085ee9a93796d353
Tarek <tarek.alkhatib1@gmail.com>
2018-06-07 14:36:55 +0200

    notFound page got style

--- frontend/src/components/not-found/notFound.js
+++ frontend/src/components/not-found/notFound.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import {Paper, Typography} from '@material-ui/core';
+import {Paper, Typography, Button} from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import './notFound.css';
 
 const styles = {
     box:{
-      background: '#fff',
       margin: '1rem auto',
       padding: '.5rem 0',
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    not404: {
+      "padding-bottom":"1rem",
+      "fontFamily": "'Gloria Hallelujah', cursive"
     }
 };
 
-const notFound = props => {
 
+const notFound = props => {
   return (
-    <div>
+    <div >
       <Paper className="main" style={styles.box} elevation={4}>
         <Typography>
-          <h1>404 Not Found</h1>
+          <h1 style={styles.not404}className="notFound">404 Not Found :<span>&#x28;</span> </h1>
+          <Link to="/">
+            <Button variant="outlined" size="large" color="primary">
+             Go Home Page
+            </Button>
+          </Link>
         </Typography>
       </Paper>
     </div>
   );
 }
-
 
 export default notFound;
