508f1e289a368888c6fb138ed190d38ec1fa015f
Tarek <tarek.alkhatib1@gmail.com>
2018-06-11 10:30:13 +0200

    trick added to notfound Page

--- frontend/src/components/not-found/notFound.js
+++ frontend/src/components/not-found/notFound.js
@@ -5,25 +5,25 @@ import './notFound.css';
 
 const styles = {
     box:{
+      backgroundColor: "lightgrey",
       margin: '1rem auto',
       padding: '.5rem 0',
       display: "flex",
       alignItems: "center",
       justifyContent: "center"
-    },
-    not404: {
-      "padding-bottom":"1rem",
-      "fontFamily": "'Gloria Hallelujah', cursive"
     }
 };
-
 
 const notFound = props => {
   return (
     <div >
       <Paper className="main" style={styles.box} elevation={4}>
         <Typography>
-          <h1 style={styles.not404}className="notFound">404 Not Found :<span>&#x28;</span> </h1>
+          <div className="hideMe" style={styles.not404}>
+            <i style={{color:"white"}} class="fab fa-snapchat-ghost notFound"></i>
+            <h1 className="notFound"> 404 </h1>
+            <h1 className="notFound">Page Not Found :<span>&#x28;</span> </h1>
+          </div>
           <Link to="/">
             <Button variant="outlined" size="large" color="primary">
              Go Home Page
