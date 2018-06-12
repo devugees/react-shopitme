1159321ebcafe565b045c1bae7541d3ac228f8a6
Nacho <nacho@nachomerino.com>
2018-06-12 12:06:59 +0200

    on it

--- frontend/src/components/nav-bar/DropMenu.js
+++ frontend/src/components/nav-bar/DropMenu.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
-import { Menu, MenuItem, Fade } from '@material-ui/core';
+import { Menu, MenuItem, Fade, Button, Chip, Avatar } from '@material-ui/core'; 
 import Avatars from './Avatars';
 import './Navbar.css';
 
 const styles = {
-  button:{padding: "0"},
+  button:{ 
+    padding: "0" 
+  }, 
   menuItem:{
     position:"absolute",
     top:"2.3rem",
     color:'black'
-  }
+  }, 
+  chip:{ 
+    margin: '0 0 0 1rem', 
+    color:'red' 
+  } 
 }
 
 export default class DropMenu extends React.Component {
@@ -31,13 +36,14 @@ export default class DropMenu extends React.Component {
     const { anchorEl } = this.state;
     return (
       <React.Fragment>
-        <Button style={styles.button}
-          aria-owns={anchorEl ? 'fade-menu' : null}
-          aria-haspopup="true"
-          onClick={this.handleClick}
-        >
-          <Avatars userName={this.props.userName}/>
-        </Button>
+        <Chip 
+          avatar={<Avatar>MB</Avatar>} 
+          label={this.props.userName} 
+          onClick={this.handleClick} 
+          aria-owns={anchorEl ? 'fade-menu' : null} 
+          aria-haspopup="true" 
+          style={styles.chip} 
+        /> 
         <Menu style={styles.menuItem}
           id="fade-menu"
           anchorEl={anchorEl}
