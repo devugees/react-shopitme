8cb2f92ec2237bbcb870bc3c6a54b61565263b69
Nacho <nacho@nachomerino.com>
2018-06-18 17:19:46 +0200

    hotfix added jibli.de link

--- frontend/src/components/aboutUs/index.js
+++ frontend/src/components/aboutUs/index.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import AddIcon from '@material-ui/icons/Add';
 import { crudAPI } from '../../helpers/helpers';
 import './aboutUs.css';
 
--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -5,7 +5,9 @@ import { year } from "../../helpers/helpers.js"
 
 const Footer = () => (
       <div className="footer">
-      <p><Link to="/about">About Us</Link> | <Link to="/">Contact </Link> &copy; {year} Jibli</p>
+      <p>
+        <Link to="/about">About Us</Link> | <Link to="/">Contact </Link> 
+        &copy; {year} <Link to="/"> Jibli.de</Link></p>
       </div>
     );
 
